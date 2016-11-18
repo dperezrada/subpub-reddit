5ef6fef897e2f2cc8ffafc909b20713c92f133e6
Rafael Avaria G <ravaria@nisum.com>
2016-11-18 19:29:30 -0300

    add api roller

--- api-poller.rb
+++ api-poller.rb
@@ -1,13 +1,14 @@
 require "redd"
+require 'json'
 
-r = Redd.it(:script, "6tR9YMaHd16Y3w", "0-dEaZy2V9NAngbw5C2DgywOHYI", "USER", "PASSWORD", user_agent: "TestBot v1.0.0")
+r = Redd.it(:script, "6tR9YMaHd16Y3w", "0-dEaZy2V9NAngbw5C2DgywOHYI", "raff_cl", "lala4life2$!", user_agent: "TestBot v1.0.0")
 r.authorize!
 
 # Streaming
 def stream_all! r
   r.stream :get_comments, "all" do |comment|
-    r.refresh_access! if r.access.expired? # for :web
-    puts comment.body
+    puts JSON.pretty_generate(comment)
+    exit
   end
 end
 
